6d5ff28560026951f73d367db14c2360b389e131
Brandon Gauthier <Brandon.L.Gauthier@gmail.com>
2013-03-09 15:32:21 -0500

    initial implementation of Standard Playing cards

---
+++ src/main/java/org/lab304/deckofcards/playingcard/PlayingCard.java
@@ -0,0 +1,35 @@
+package org.lab304.deckofcards.playingcard;
+
+import org.apache.commons.lang.builder.*;
+import org.lab304.deckofcards.Card;
+
+/**
+ * A typical playing card that is found in a 52 card deck.
+ *
+ * Note: This class intentionally lacks setter as cards should not be able to change their suit or faceValue.
+ */
+public class PlayingCard implements Card {
+    private Suit suit;
+    private FaceValue faceValue;
+
+    public PlayingCard(FaceValue faceValue, Suit suit) {
+        this.suit = suit;
+        this.faceValue = faceValue;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public FaceValue getFaceValue() {
+        return faceValue;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).
+                append("suit", suit).
+                append("faceValue", faceValue).
+                toString();
+    }
+}
---
+++ src/main/java/org/lab304/deckofcards/playingcard/Suit.java
@@ -0,0 +1,11 @@
+package org.lab304.deckofcards.playingcard;
+
+/**
+ * The standard Suits of playing cards.
+ */
+public enum Suit {
+    HEARTS,
+    SPADES,
+    CLUBS,
+    DIAMONDS
+}
