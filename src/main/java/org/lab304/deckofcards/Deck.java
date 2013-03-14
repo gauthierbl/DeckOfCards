4219bdc61c0dff37801a7e3feecba7e7c2f7648a
Brandon Gauthier <Brandon.L.Gauthier@gmail.com>
2013-03-14 14:44:09 -0400

    implemented the shuffle() and the dealOneCard() methods

--- src/main/java/org/lab304/deckofcards/Deck.java
+++ src/main/java/org/lab304/deckofcards/Deck.java
@@ -1,24 +1,38 @@
 package org.lab304.deckofcards;
 
+import org.lab304.util.Shuffler;
+
+import java.util.Stack;
+
 /**
- * A Deck is a set of {@link Card}s the provides two built in methods.
+ * A Deck is a collection of {@link Card}s the provides two built in methods.
  */
 public class Deck {
+
+    Stack<Card> cards = new Stack<Card>();
 
     /**
      * Shuffles the Deck, insuring that the {@link Card}s are in a random order.
      */
     public void shuffle() {
-        // TODO implement me! Use Fisher–Yates shuffle
+
+        Shuffler shuffler = new Shuffler();
+
+        shuffler.shuffle(cards);
     }
 
     /**
-     * Deals one card from the top of the Deck.
+     * Deals one card from the top of the Deck. If there is no card to deal then return null;
      *
-     * @return the top {@link Card} in the Deck
+     * @return the top {@link Card} in the Deck, or null if there are no cards left to deal.
      */
     public Card dealOneCard() {
-        // TODO implement me!
-        return null;
+        Card result = null;
+
+        if (!cards.empty()) {
+            cards.pop();
+        }
+
+        return result;
     }
 }
